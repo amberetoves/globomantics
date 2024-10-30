6f2583fa02ed4e619414c9b716fbeb69c425b74f
Amber Toves <amberetoves@gmail.com>
2024-10-30 15:17:51 +1000

    API: Creating a Database; misc changes

--- Api/obj/Debug/net8.0/Api.AssemblyInfo.cs
+++ Api/obj/Debug/net8.0/Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+77992ad3d45912b3c666a0098776b8e1641e2700")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f28cb401a7013f2cd8225223348e86af38cf551")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
